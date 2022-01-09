c02923edc82bcafe294104124ee95641fb61a4ae
nabilAparicio <orlando.nabil@gmail.com>
2022-01-09 13:24:05 -0500

    debug webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const { cleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -54,7 +54,7 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css'
         }),
-        new cleanWebpackPlugin(),
+        new CleanWebpackPlugin(),
     ],
     optimization:{
         minimize: true,
